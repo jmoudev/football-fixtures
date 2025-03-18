a899446acc8205b0f16c537c128eeab360d1b9f7
Joe Mould <64282147+jmoudev@users.noreply.github.com>
2025-03-18 00:07:44 +0000

    Adds errors and tests

--- src/fixtures/main.py
+++ src/fixtures/main.py
@@ -4,25 +4,42 @@ from typing_extensions import Annotated
 from requests_cache import CachedSession
 import typer
 
+
+class InvalidTeamError(Exception):
+    pass
+
+
+class NoFixturesError(Exception):
+    pass
+
+
+FPL_BASE_URL = "https://fantasy.premierleague.com/api/"
+TEAMS_URL = f"{FPL_BASE_URL}/bootstrap-static/"
+FIXTURES_URL = f"{FPL_BASE_URL}/fixtures/"
+
+
 app = typer.Typer(add_completion=False)
 
 
 def _get_teams(session):
-    response = session.get("https://fantasy.premierleague.com/api/bootstrap-static/")
+    response = session.get(TEAMS_URL)
     response.raise_for_status()
     teams = {team["name"]: team["id"] for team in response.json()["teams"]}
     return teams
 
 
 def _get_fixtures(session):
-    response = session.get("https://fantasy.premierleague.com/api/fixtures/")
+    response = session.get(FIXTURES_URL)
     response.raise_for_status()
     fixtures = response.json()
     return fixtures
 
 
 def _get_id_from_team(team, teams):
-    return teams[team.capitalize()]
+    try:
+        return teams[team.capitalize()]
+    except KeyError as e:
+        raise InvalidTeamError(f"Invalid team: {team}") from e
 
 
 def _get_team_from_id(team_id, teams):
@@ -52,6 +69,8 @@ def main(team: str, results: Annotated[bool, typer.Option()] = False):
         recent_fixtures = [fixture for fixture in team_fixtures if fixture["finished"]][
             -5:
         ]
+        if len(recent_fixtures) == 0:
+            raise NoFixturesError(f"No recent fixtures found for '{team}'")
         typer.echo("Results:")
         for fixture in recent_fixtures:
             fixture_string = f"- {_get_team_from_id(fixture['team_h'], teams)} {fixture['team_h_score']} {_get_team_from_id(fixture['team_a'], teams)} {fixture['team_a_score']}, {_get_readable_datetime(fixture['kickoff_time'])}"
@@ -60,6 +79,8 @@ def main(team: str, results: Annotated[bool, typer.Option()] = False):
         upcoming_fixtures = [
             fixture for fixture in team_fixtures if not fixture["finished"]
         ][:5]
+        if len(upcoming_fixtures) == 0:
+            raise NoFixturesError(f"No upcoming fixtures found for '{team}'")
         typer.echo("Upcoming Fixtures:")
         for fixture in upcoming_fixtures:
             fixture_string = f"- {_get_team_from_id(fixture['team_h'], teams)} vs {_get_team_from_id(fixture['team_a'], teams)}, {_get_readable_datetime(fixture['kickoff_time'])}"
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,11 +1,99 @@
+import pytest
 from typer.testing import CliRunner
 
-from fixtures.main import app
+import fixtures.main
+from fixtures.main import InvalidTeamError, NoFixturesError, app
 
 runner = CliRunner()
 
 
-def test_app():
-    result = runner.invoke(app, ["Liverpool"])
+@pytest.fixture
+def mock_teams(monkeypatch):
+    def mock_get_teams(*args, **kwargs):
+        return {
+            "London": 1,
+            "Glasgow": 2,
+            "Cardiff": 3,
+            "Belfast": 4,
+            "Edinburgh": 5,
+        }
+
+    monkeypatch.setattr(fixtures.main, "_get_teams", mock_get_teams)
+
+
+@pytest.fixture
+def mock_fixtures(monkeypatch):
+    def mock_get_fixtures(*args, **kwargs):
+        return [
+            {
+                "finished": True,
+                "team_h": 1,
+                "team_h_score": 0,
+                "team_a": 2,
+                "team_a_score": 0,
+                "kickoff_time": "2025-01-01T09:00:00Z",
+            },
+            {
+                "finished": True,
+                "team_h": 3,
+                "team_h_score": 2,
+                "team_a": 1,
+                "team_a_score": 1,
+                "kickoff_time": "2025-02-01T12:00:00Z",
+            },
+            {
+                "finished": False,
+                "team_h": 1,
+                "team_h_score": None,
+                "team_a": 4,
+                "team_a_score": None,
+                "kickoff_time": "2025-03-01T15:00:00Z",
+            },
+            {
+                "finished": False,
+                "team_h": 2,
+                "team_h_score": None,
+                "team_a": 1,
+                "team_a_score": None,
+                "kickoff_time": "2025-04-01T18:00:00Z",
+            },
+        ]
+
+    monkeypatch.setattr(fixtures.main, "_get_fixtures", mock_get_fixtures)
+
+
+def test_fixtures(mock_teams, mock_fixtures):
+    result = runner.invoke(app, ["London"])
     assert result.exit_code == 0
     assert "Upcoming Fixtures:" in result.stdout
+    assert "- London vs Belfast, 01 Mar 15:00" in result.stdout
+    assert "- Glasgow vs London, 01 Apr 18:00" in result.stdout
+
+
+def test_valid_results(mock_teams, mock_fixtures):
+    result = runner.invoke(app, ["London", "--results"])
+    assert result.exit_code == 0
+    assert "Results:" in result.stdout
+    assert "- London 0 Glasgow 0, 01 Jan 09:00" in result.stdout
+    assert "- Cardiff 2 London 1, 01 Feb 12:00" in result.stdout
+
+
+def test_invalid_team(mock_teams, mock_fixtures):
+    result = runner.invoke(
+        app,
+        ["NotLondon"],
+    )
+    assert result.exit_code == 1
+    assert isinstance(result.exception, InvalidTeamError)
+
+
+def test_no_fixtures(mock_teams, mock_fixtures):
+    result = runner.invoke(app, ["Edinburgh"])
+    assert result.exit_code == 1
+    assert isinstance(result.exception, NoFixturesError)
+
+
+def test_no_results(mock_teams, mock_fixtures):
+    result = runner.invoke(app, ["Edinburgh", "--results"])
+    assert result.exit_code == 1
+    assert isinstance(result.exception, NoFixturesError)
